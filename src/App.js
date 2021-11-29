391c404d4370916960b36df78d8ae45eedd01ba6
Wekiban <wes.banyard@googlemail.com>
2021-11-29 16:15:00 +0100

    api issue

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import MapComponent from './Components/MapComponent';
 import { validateIPaddress } from './HelperFunctions/validateIpAddress';
 import Theme from './Theme';
 
-const API_KEY = process.env.REACT_API_KEY;
+let API_KEY;
 
 function App() {
   // variable to check if this is the first time app is loading
