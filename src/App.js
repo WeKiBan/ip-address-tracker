fd8c292c308b90197befc9355077d1a949e8bfd3
Wekiban <wes.banyard@googlemail.com>
2021-11-29 13:19:55 +0100

    api key

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import MapComponent from './Components/MapComponent';
 import { validateIPaddress } from './HelperFunctions/validateIpAddress';
 import Theme from './Theme';
 
-const API_KEY = 'fb5060ecc988c3db7cf4a91a4df9120d';
+const API_KEY = process.env.REACT_API_KEY;
 
 function App() {
   // variable to check if this is the first time app is loading
