f96dfaab99770e0d8cd42aca834b783798b7de00
Wekiban <wes.banyard@googlemail.com>
2021-11-30 14:16:36 +0100

    fix issue with full screen

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
 
   useEffect(() => {
     const updateHeight = () => {
-      setWindowHeight(window.innerHeight);
+      setWindowHeight(window.visualViewport.height);
     };
     window.addEventListener('resize', updateHeight);
 
